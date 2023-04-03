e39e19ba2b2d9b3a455d5830b9531d5b01543518
Vijay Mamilla <vijay.mamilla@gmail.com>
2023-04-03 09:14:18 -0400

    Added logic to handle pagination records

--- scrapy/de_spider/de_spider/items.py
+++ scrapy/de_spider/de_spider/items.py
@@ -1,12 +1,11 @@
-# Define here the models for your scraped items
-#
-# See documentation in:
-# https://docs.scrapy.org/en/latest/topics/items.html
 
-import scrapy
+from scrapy.item import Item, Field
 
-
-class DeSpiderItem(scrapy.Item):
+class DeSpiderItem(Item):
     # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+    price = Field()
+    location = Field()
+    num_bed_rooms = Field()
+    num_bath_rooms = Field()
+    area = Field()
+    building_type = Field()
--- scrapy/de_spider/de_spider/spiders/de_spider.py
+++ scrapy/de_spider/de_spider/spiders/de_spider.py
@@ -1,38 +1,57 @@
 import scrapy
-from csv import writer
-
-filename = 'output/housing.csv'
-
+from ..items import DeSpiderItem
+from scrapy.spidermiddlewares.httperror import HttpError
+from twisted.internet.error import DNSLookupError
+from twisted.internet.error import TimeoutError, TCPTimedOutError
 
 class DataExtractionSpider(scrapy.Spider):
     name = "de_spider"
     start_urls = ["https://www.bproperty.com/en/bangladesh/properties-for-sale/"]
     website_main_url = "https://www.bproperty.com/"
-    header = "'Type', 'Location', 'Price', 'Area', 'Bed Rooms', 'Bath Rooms'"
-    with open(filename, 'a+') as f:
-        f.write(header + "\n")
 
+    #
     def parse(self, response):
+        self.logger.info('Parse function called on %s', response.url)
 
-        urls = response.css("li article div a._287661cb::attr(href)").getall()
+        url_context_names = response.css("li article div a._287661cb::attr(href)").getall()
 
-        urls = [ self.website_main_url+context_name for context_name in urls]
+        current_url_list = [self.website_main_url + context_name for context_name in url_context_names]
 
-        for url in urls:
-            yield scrapy.Request(url=url, callback=self.parse_details_page)
+        for url in current_url_list:
+            yield scrapy.Request(url=url, callback=self.parse_details_page, errback=self.errback_httpbin)
+
+        next_page = response.xpath('//div/ul/li/a[contains(@title, "Next")]').xpath('@href').get()
+
+        if next_page is not None:
+
+            new_url = self.website_main_url+next_page
+            yield response.follow(url=new_url, callback=self.parse,errback = self.errback_httpbin)
+
 
     def parse_details_page(self, response):
 
-        price = response.css("span._105b8a67::text").get()
-        location = response.css("div._1f0f1758::text").get()
-        num_bed_rooms = response.css("span.fc2d1086::text").get()
-        num_bath_rooms = response.css("span.fc2d1086::text").get()
-        area = response.css("span.fc2d1086::text").get()
-        building_type = response.css("span._812aa185::text").get()
+        item = DeSpiderItem()
+        item['price'] = response.css("span._105b8a67::text").get()
+        item['location'] = response.css("div._1f0f1758::text").get()
+        item['num_bed_rooms'] = response.css("span.fc2d1086::text").get()
+        item['num_bath_rooms'] = response.css("span.fc2d1086::text").get()
+        item['area'] = response.css("span.fc2d1086::text").get()
+        item['building_type'] = response.css("span._812aa185::text").get()
 
-        info = f"{building_type}, {location}, {price}, {area}, {num_bed_rooms}, {num_bath_rooms}"
-        #yield  { "type":building_type,"location": location, "price":price, "area": area,"num_bed_rooms": num_bed_rooms, "num_bath_rooms":num_bath_rooms}
+        yield item
 
+    def errback_httpbin(self, failure):
+        # logs failures
+        self.logger.error(repr(failure))
 
-        with open(filename, 'a+') as f:
-            f.write(info + "\n")
+        if failure.check(HttpError):
+            response = failure.value.response
+            self.logger.error("HttpError occurred on %s", response.url)
+
+        elif failure.check(DNSLookupError):
+            request = failure.request
+            self.logger.error("DNSLookupError occurred on %s", request.url)
+
+        elif failure.check(TimeoutError, TCPTimedOutError):
+            request = failure.request
+            self.logger.error("TimeoutError occurred on %s", request.url)
